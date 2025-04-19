047f3529428b5368b0ca9e0d7e9505bb6df1369f
DylanBk <d.bullock1404@outlook.com>
2025-04-19 13:46:27 +0100

    prepared backend for Render

--- backend/modules/__init__.py
+++ backend/modules/__init__.py
@@ -2,5 +2,6 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request, send_from_directory, session
 from flask_cors import CORS
+import os
 
 from . import db_functions as db
