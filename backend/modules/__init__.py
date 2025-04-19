5620299a455d3c64a5a8e76e4a55bbf076c29ebd
DylanBk <d.bullock1404@outlook.com>
2025-04-19 13:06:18 +0100

    prepared back end for railway deploy

--- backend/modules/__init__.py
+++ backend/modules/__init__.py
@@ -2,6 +2,5 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request, send_from_directory, session
 from flask_cors import CORS
-import os
 
 from . import db_functions as db
